23c679e57977c4802e7b8aea9941ebc5371f30dd
Jarid <jaridpoe@gmail.com>
2020-09-28 14:40:43 -0400

    add launch options

--- test/example.test.js
+++ test/example.test.js
@@ -1,11 +1,15 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-describe('My first puppeteer test', () => {
-  it('should launch the browser?', async () => {
-    const browser = await puppeteer.launch({headless: false});
+describe("My first puppeteer test", () => {
+  it("should launch the browser?", async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 10,
+      devtools: false,
+    });
     const page = await browser.newPage();
-    await page.goto('http://example.com');
-    await page.waitForSelector('h1');
+    await page.goto("http://example.com");
+    await page.waitForSelector("h1");
     await browser.close();
-  })
+  });
 });
